77bf6e415e24bcefdc0378f4750afe8d8a9444c7
Jadyn Coleman <Jadyncoleman04 @gmail.com>
2025-02-28 17:09:36 -0500

    deploy updated build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import './App.css';
-import Homepage from './homepage'
+import Homepage from './homepage';
 function App() {
   return (
     <div className="App">
